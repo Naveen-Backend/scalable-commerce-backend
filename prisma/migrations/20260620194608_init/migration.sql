9b48dbca4211efaf9356db6710c8092e07b3fcb8
Naveen Telagathoti <telagathotinaveen09@gmailcom>
2026-06-21 10:55:08 -0700

    chore: add prisma migration files

---
+++ prisma/migrations/20260620194608_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_enail_key" ON "users"("email");
